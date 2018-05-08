2dd88288ee897d005a1771f7f5b6492b153c71d0
Your Name <you@example.com>
2018-05-08 10:33:27 +0800

    Finish part1

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -10,24 +10,25 @@
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
+struct convert : public thrust::unary_function<char, int> {
+	__host__ __device__
+	int operator()(char c)
+	{
+		if (c == '\n') return 0;
+		return 1;
+	}
+};
+
 void CountPosition1(const char *text, int *pos, int text_size)
 {
-	struct convert : public thrust::unary_function<char, int> {
-		__host__ __device__
-		int operator()(char c)
-		{
-			if (c == '\n') return 0;
-			return 1;
-		}
-	};
 	thrust::equal_to<int> binary_pred;
 	thrust::plus<int> binary_op;
 	thrust::device_ptr<const char> dev_ptr1(text);
 	thrust::device_ptr<int> dev_ptr2(pos);
 	/*    Replace space to 0, others to 1    */
-	thrust::transform(thrust::host, dev_ptr1, dev_ptr1 + text_size, dev_ptr2, convert());
+	thrust::transform(thrust::device, dev_ptr1, dev_ptr1 + text_size, dev_ptr2, convert());
 	/*    Segment prefix sum all position    */
-	//thrust::inclusive_scan_by_key(thrust::host, dev_ptr2, dev_ptr2 + text_size, dev_ptr2, dev_ptr2, binary_pred, binary_op);
+	thrust::inclusive_scan_by_key(thrust::device, dev_ptr2, dev_ptr2 + text_size, dev_ptr2, dev_ptr2, binary_pred, binary_op);
 }
 
 void CountPosition2(const char *text, int *pos, int text_size)
